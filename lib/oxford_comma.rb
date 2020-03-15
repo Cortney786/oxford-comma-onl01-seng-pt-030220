d68ccb0f3971af014e845631d2af202f1c930e31
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-15 23:52:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
     if array.length == 2
-        array.join(" and ")
+        array.join(" , and ")
     elsif array.length > 2
         array.insert(-2, "and")
-        array[0..-1].join(",")
+        array[0..-1].join(" ")
     else
         array.join
     end
