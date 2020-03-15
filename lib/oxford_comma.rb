1a9e449ab1e842fc7056ed763c9ba1aff1b5773b
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-15 23:56:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,11 @@
 def oxford_comma(array)
-    if array.length == 3
+    if array.length == 2
         array.join(" and ")
-    elsif array.length > 3
-        array.insert(-3, "and")
-        array[0..-3].join(",")
+    elsif array.length > 2
+        array.insert(-2, "and")
+        array[0..-1].join(", ")
     else
         array.join
     end
 end
-
-
 
